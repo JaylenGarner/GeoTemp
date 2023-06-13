acc60914d927fc2845302aa90e2e904a0ad38519
Jaylen Garner <jaylen.garner@outlook.com>
2023-06-12 23:20:51 -0400

    Images staged for all weather categories

--- src/app/page.js
+++ src/app/page.js
@@ -9,21 +9,21 @@ import Cards from "@/components/Cards";
 const Home = () => {
   const address = useRecoilValue(addressState)
   const weather = useRecoilValue(weatherState)
-  const [background, setBackground] = useState('bg-view')
+  const [background, setBackground] = useState('bg-drizzle')
 
   useEffect(() => {
-
+    console.log(weather)
   }, [address, weather])
 
   return (
     <div className={`flex flex-grow flex-col align-center ${background} bg-center bg-cover p-6 text-center`}>
-      {address ? <h1 className="text-center mt-14 text-5xl font-extrabold leading-[3.5rem]">{address}</h1> :
+      {address ? <h1 className="text-center mt-14 text-6xl font-extrabold leading-[3.5rem]">{address}</h1> :
       <h1 className="mt-14 text-5xl font-bold leading-[3.5rem]">Choose a location to check the weather</h1>}
 
       {address && weather && (
         <>
-        <h2 className="mt-20 text-5xl font-semibold ">{weather.current.weather[0].main}</h2>
-        <h2 className="mt-6 text-5xl font-semibold">{weather.current.temp}</h2>
+        <h2 className="mt-10 text-5xl font-semibold ">{weather.current.weather[0].description.charAt(0).toUpperCase() + weather.current.weather[0].description.slice(1)}</h2>
+        <h2 className="mt-6 text-5xl font-semibold">{weather.current.temp.toFixed(0)} °F</h2>
         <h2 className="mt-6 text-5xl font-semibold leading-[3.5rem]">Humidity: {weather.current.humidity}%</h2>
         <h2 className="mt-6 text-5xl font-semibold leading-[3.5rem]">Wind: {weather.current.wind_speed} mph</h2>
         <Cards />
