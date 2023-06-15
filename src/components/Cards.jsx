b918843cd02faa058d85ffa3c3cd8e7a6cb9318f
Jaylen Garner <jaylen.garner@outlook.com>
2023-06-15 19:09:03 -0400

    5 day forecast is populating

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,45 +1,22 @@
-import getIcon from "../../lib/getIcon"
-import Image from "next/image"
-
-let test = [
-    {day: 'Monday',
-    weather: 'Sunny',
-    temp: 85
-    },
-    {day: 'Tuesday',
-    weather: 'Rain',
-    temp: 67},
-    {day: 'Wednesday',
-    weather: 'Cloudy',
-    temp: 72},
-    {day: 'Thursday',
-    weather: 'Snow',
-    temp: 43},
-    {day: 'Friday',
-    weather: 'Sunny',
-    temp: 82}
-]
+import { useRecoilValue } from "recoil";
+import { weatherState } from "../../atoms/weatherAtom";
+import getDay from "../../lib/getDay";
 
 const Cards = () => {
+  const weather = useRecoilValue(weatherState)
 
   return <>
   {/* Desktop Navigation */}
     <div className="grid grid-rows-1 auto-cols-fr grid-flow-col gap-4 w-full bottom-0 text-center mt-14 p-4 ">
-        {test.map((day) => {
-
+        {weather.daily.slice(1, 6).map((day) => {
             return (
-              <div key={day.day} className="border h-fit w-full flex flex-col pb-6 bg-gray-800 rounded-xl text-2xl">
-               <h3 className=" pr-2 pl-2 pb-5 pt-4 font-bold ">{day.day}</h3>
-              <div className="flex justify-center pb-3">
-                <Image
-                  alt="Todays weather"
-                  src={getIcon(day.weather)}
-                  height={60}
-                  width={60}
-                />
+              <div key={day.dt} className="border h-fit w-full flex flex-col pb-6 bg-gray-800 rounded-xl text-2xl">
+               <h3 className=" pr-2 pl-2 pt-4 font-bold ">{getDay(day.dt)}</h3>
+              <div className="flex justify-center">
+                <img src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}></img>
               </div>
-              <p className=" font-semibold p-2">{day.weather}</p>
-              <p className=" font-bold">{day.temp}°F</p>
+               <p className=" font-semibold pb-2">{day.weather[0].main}</p>
+              <p className=" font-bold">{day.temp.max.toFixed(0)} °F</p>
             </div>
         )})}
     </div>
