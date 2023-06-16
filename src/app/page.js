57317b5af039ba81cdb373f0d85500d1e5361baf
Jaylen Garner <jaylen.garner@outlook.com>
2023-06-15 20:18:30 -0400

    Mobile responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -16,21 +16,29 @@ const Home = () => {
 
   return (
     <div
-    className={`flex flex-grow flex-col align-center bg-cover bg-center p-6 text-center`}
+    className={`flex flex-grow flex-col bg-cover bg-center p-6 text-center justify-between`}
     style={{backgroundImage: `url(/weather/${weather?.current.weather[0].main.toLowerCase()}.jpg)`}}
      >
-      {address ? <h1 className="text-center mt-14 text-6xl font-extrabold leading-[3.5rem]">{address}</h1> :
-      <h1 className="mt-14 text-5xl font-bold leading-[3.5rem]">Choose a location to check the weather</h1>}
+
+      {/* For spacing elements with flex */}
+      {address && weather && <span> </span>}
+
+       <div>
+      {address ? <h1 className="text-center text-6xl font-extrabold leading-[4.5rem]">{address}</h1> : (
+        <h1 className="mt-8 text-6xl font-bold leading-[3.5rem]">What's the weather?</h1>
+      )}
 
       {address && weather && (
         <>
-        <h2 className="mt-10 text-5xl font-semibold ">{weather.current.weather[0].description.charAt(0).toUpperCase() + weather.current.weather[0].description.slice(1)}</h2>
-        <h2 className="mt-6 text-5xl font-semibold">{weather.current.temp.toFixed(0)} °F</h2>
-        <h2 className="mt-6 text-5xl font-semibold leading-[3.5rem]">Humidity: {weather.current.humidity}%</h2>
-        <h2 className="mt-6 text-5xl font-semibold leading-[3.5rem]">Wind: {weather.current.wind_speed} mph</h2>
-        <Cards />
-      </>
+        <h2 className="mt-6 text-5xl font-semibold ">{weather.current.weather[0].description.charAt(0).toUpperCase() + weather.current.weather[0].description.slice(1)}</h2>
+        <h2 className="mt-5 text-5xl font-semibold">{weather.current.temp.toFixed(0)} °F</h2>
+        <h2 className="mt-5 text-5xl font-semibold leading-[3.5rem]">Humidity: {weather.current.humidity}%</h2>
+        <h2 className="mt-5 text-5xl font-semibold leading-[3.5rem]">Wind: {weather.current.wind_speed} mph</h2>
+        </>
       )}
+      </div>
+
+      {address && weather && <Cards />}
     </div>
   );
 };
